a01ace34e93dec0f97865a5de6d88d1a17becad1
JJavierGarcia <hnd85543@educastur.es>
2024-12-16 16:46:29 +0100

    Update CommandExec-1.php

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -22,9 +22,9 @@
   <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
     <?php
     if(isset($_GET["username"])){
+      echo shell_exec($user);
       $user = $_GET["username"];
       $pass = $_GET["password"];
-      echo shell_exec($user);
       if($user == "Admin" && $pass == "ufoundmypassword")
         echo "WELLDONE";
     }
