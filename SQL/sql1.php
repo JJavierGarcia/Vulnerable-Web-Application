1e1395df346b2979a104b3e9f89bf17017f5bc56
JJavierGarcia <hnd85543@educastur.es>
2024-12-19 17:06:34 +0100

    Update sql1.php
    secure pass

--- SQL/sql1.php
+++ SQL/sql1.php
@@ -27,6 +27,7 @@
 	$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
 	// Create connection
+	$password = getenv('secure_pass');
 	$conn = mysqli_connect($servername,$username,$password,$db);
 
 	// Check connection
